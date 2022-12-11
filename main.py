0d03445a42d5f92963ee7a2f611e5b139fc86902
gajendra-github <gajendra.github@gmail.com>
2022-12-10 19:58:18 -0500

    added git commands inside comment

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ print('Python 101!!!')
 
 print('New York City')
 
+'''
+$git config --global user.name "gajendra-github"
+$git config --global user.email "gajendra.github@gmail.com"
+$git add .
+$git status
+$git commit -m "first commit"
+$git branch -M main
+$git remote add origin https://github.com/gajendra-github/python-demo.git
+$git push -u origin main
+
+
+
+
+
+
+
+'''
