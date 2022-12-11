907e41f742d5da99d284ea06d51f728c67e0ee93
gajendra-github <gajendra.github@gmail.com>
2022-12-10 22:09:54 -0500

    added STRING folder/replace_word.py and resources.txt

---
+++ STRING/replace_word.py
@@ -0,0 +1,10 @@
+def replace_word():
+    str = "I Love New York City...The Best City In The World"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word to replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+
+replace_word()
+
+
